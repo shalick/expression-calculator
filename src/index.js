6567d325b4acaa2a15e3611fb68729d68c81d526
Aliaksandr Shabanovich <alikshabanovich@gmail.com>
2021-08-31 02:26:55 +0300

    feat: do expression-calculator task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,26 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.split(' ').join('');
+
+    let brackets = 0;
+
+    for (const el of expr) {
+        if (el === '(') {
+            brackets++;
+        } else if (el === ')') {
+            brackets--;
+        }
+    }
+      
+    if (brackets) {
+        throw new Error('ExpressionError: Brackets must be paired');
+    }
+    if (expr.includes('/0')) {
+        throw new Error('TypeError: Division by zero.');
+    }
+
+    return new Function(`return ${expr}`)();
 }
 
 module.exports = {
